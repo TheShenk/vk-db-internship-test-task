8e72b4802171d324ca642a271d0190f8eb210d97
TheShenk <udod_maksim@mail.ru>
2022-04-05 10:09:57 +0300

    feat: base file transfer

--- client.c
+++ client.c
@@ -5,11 +5,22 @@
 #include <stdio.h>
 
 #include <sys/socket.h>
+#include <sys/sendfile.h>
 #include <arpa/inet.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <string.h>
 #include "common_structs.h"
 
 int main(int argc, char  **argv) {
+
+    char *filename = "test.txt";
+
     int sock_d = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock_d < 0) {
+        printf("Error: can't create socket - %d\n", errno);
+        return sock_d;
+    }
 
     struct sockaddr_in sock_addr = {
             .sin_family = AF_INET,
@@ -19,18 +30,40 @@ int main(int argc, char  **argv) {
 
     int err = connect(sock_d, (struct sockaddr *)&sock_addr, sizeof(sock_addr));
     if (err < 0) {
-        printf("Error: can't connect to server - %d", err);
+        printf("Error: can't connect to server - %d\n", errno);
+        return err;
     }
+
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        printf("Error: can't open file\n");
+        return -1;
+    }
+
+    int file_d = fileno(file);
+
+    struct stat buf;
+    fstat(file_d, &buf);
+    off_t size = buf.st_size;
 
     struct init_msg msg = {
-            .save_filename = "hello_world.txt",
-            .file_size = 300
+            .file_size = size
     };
 
-    err = send(sock_d, &msg, sizeof(struct init_msg), 0);
-    if (err < 0) {
-        printf("Error: can't send data - %d", err);
+    strncpy(msg.save_filename, filename, MAX_FILENAME_LEN-1);
+    msg.save_filename[MAX_FILENAME_LEN-1] = '\0';
+
+    ssize_t init_sent_size = send(sock_d, &msg, sizeof(struct init_msg), 0);
+    if (init_sent_size < 0) {
+        printf("Error: can't send init data - %d\n", errno);
+        return init_sent_size;
     }
+
+    off_t offset = 0;
+    ssize_t sent_size = sendfile(sock_d, file_d, &offset, size);
+    printf("Sent %zd bytes\n", sent_size);
+
+    fclose(file);
 
     return 0;
 }
--- server.c
+++ server.c
@@ -6,7 +6,10 @@
 
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <errno.h>
 #include "common_structs.h"
+
+#define READ_DATA_CHUNK 255
 
 int main(int argc, char  **argv) {
     int sock_d = socket(AF_INET, SOCK_STREAM, 0);
@@ -25,13 +28,13 @@ int main(int argc, char  **argv) {
 
     int err = bind(sock_d, (struct sockaddr *)&addr, sizeof(addr));
     if (err < 0) {
-        printf("Error: can't bind socket - %d", err);
+        printf("Error: can't bind socket - %d", errno);
         return err;
     }
 
     err = listen(sock_d, 1);
     if (err < 0) {
-        printf("Error: can't listen - %d", err);
+        printf("Error: can't listen - %d\n", errno);
         return err;
     }
 
@@ -41,14 +44,43 @@ int main(int argc, char  **argv) {
     while (1) {
         int conn_sock_d = accept(sock_d, NULL, NULL);
         if (conn_sock_d < 0) {
-            printf("Error: can't accept - %d", err);
+            printf("Error: can't accept - %d\n", errno);
             continue;
         }
 
         struct init_msg msg;
-        recv(conn_sock_d, &msg, sizeof(struct init_msg), 0);
+        ssize_t recv_size = recv(conn_sock_d, &msg, sizeof(struct init_msg), 0);
+        if (recv_size < 0) {
+            printf("Error: can't read init msg - %d\n", errno);
+            continue;
+        }
 
-        printf("From client: file name - %s, file size - %d\n", msg.save_filename, msg.file_size);
+        FILE  *file = fopen(msg.save_filename, "w");
+        if (!file) {
+            printf("Error: can't open file - %d\n", errno);
+            continue;
+        }
+
+        unsigned int total_read_size = 0;
+        char data_chunk[READ_DATA_CHUNK] = {0};
+
+        while (total_read_size < msg.file_size) {
+            size_t read_size = recv(conn_sock_d, data_chunk, READ_DATA_CHUNK, 0);
+            if (read_size < 0) {
+                printf("Error: can't read data - %d\n", errno);
+                break;
+            }
+
+            int write_size = fwrite(data_chunk, read_size, 1, file);
+            if (write_size < 0) {
+                printf("Error: can't write to file - %d\n", errno);
+                break;
+            }
+            total_read_size += read_size;
+        }
+
+        fclose(file);
+        printf("From client: file name - %s, file size - %d, read - %d\n", msg.save_filename, msg.file_size, total_read_size);
     }
 
     return 0;
